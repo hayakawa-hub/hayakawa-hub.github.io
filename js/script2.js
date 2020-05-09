30cb16ab94b5e4410ddb7083f0cc891086946680
hayakawa-hub <r.h6526@gmail.com>
2020-05-09 13:20:39 +0900

    更新

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,19 @@
 $(function(){
-  $.simpleTicker($('#marquee_wrap'),{'effectType':'roll'});
-});
+  var interval =7000;
+
+  var duration = 2000;
+
+
+  var timer;
+
+  timer =  setInterval(slideTimer,interval);
+
+  function slideTimer(){
+    $("#mawasu ul").animate({"left":"-=844px"},duration,
+  function(){
+    $(this).append($("#mawasu li:first-child"));
+
+    $(this).css("left",-1000);
+  });
+  }
+})
