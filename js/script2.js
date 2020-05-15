910164945d4ab55b4bd586a3d185988b9e798ef7
hayakawa-hub <r.h6526@gmail.com>
2020-05-15 20:49:30 +0900

    完成版

--- js/script2.js
+++ js/script2.js
@@ -1,19 +1,3 @@
 $(function(){
-  var interval =7000;
-
-  var duration = 2000;
-
-
-  var timer;
-
-  timer =  setInterval(slideTimer,interval);
-
-  function slideTimer(){
-    $("#mawasu ul").animate({"left":"-=844px"},duration,
-  function(){
-    $(this).append($("#mawasu li:first-child"));
-
-    $(this).css("left",-1000);
-  });
-  }
-})
+  $.simpleTicker($("#mawasu"),{"effectType":"slide"});
+});
